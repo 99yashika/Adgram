f21724f14ded42978907f0d912d054033aa79d9c
99yashika <38666122+99yashika@users.noreply.github.com>
2020-07-30 12:22:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adgram_app/welcomePage.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_adgram_app/userScreen.dart';
 
 
 
@@ -61,10 +62,10 @@ class _HomePageState extends State<HomePage> {
 
   Widget _advertiserButton() {
     return InkWell(
-//      onTap: () {
-//        Navigator.push(
-//            context, MaterialPageRoute(builder: (context) => SignUpPage()));
-//      },
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyHomePage()));
+      },
       child: Container(
 //        width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.symmetric(vertical: 13),
